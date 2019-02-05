12e9f8b356a85c2e6dc98e6f01e96ea5a593f43b
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2019-02-05 07:03:07 +0200

    v0.0.1

--- test/tests/index.js
+++ test/tests/index.js
@@ -2,7 +2,7 @@ import index from '../../src'
 
 describe('index', function () {
 	it('base', function () {
-		console.log(JSON.stringify(index.args, null, 4))
+		// console.log(JSON.stringify(index.args, null, 4))
 		assert.ok(index.args)
 		assert.ok(index.args._)
 	})
