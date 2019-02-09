4a741a9d3e589efe98a6e1084def8436a7b1d471
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2019-02-09 08:01:11 +0200

    Helpers for browser

---
+++ .babelrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "presets": [
+    "@babel/preset-env"
+  ],
+  "plugins": [
+    "@babel/plugin-transform-runtime",
+    "@babel/plugin-syntax-dynamic-import",
+    "@babel/plugin-proposal-optional-chaining",
+    "@babel/plugin-proposal-throw-expressions",
+	
+    ["@babel/plugin-proposal-class-properties", { "loose": true }],
+    "@babel/plugin-transform-parameters",
+    "@babel/plugin-transform-async-to-generator"
+  ]
+}
