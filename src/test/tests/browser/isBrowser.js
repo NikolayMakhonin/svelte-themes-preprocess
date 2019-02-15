967f76cf7217b9cb8bb8705636e0b129c48252c6
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2019-02-15 08:41:39 +0200

    Fix tests descriptions

--- src/test/tests/browser/isBrowser.js
+++ src/test/tests/browser/isBrowser.js
@@ -1,8 +1,10 @@
 /* eslint-disable no-new-func */
 
-it('isBrowser', function () {
-	// see: https://stackoverflow.com/a/31090240/5221762
-	const isBrowser = new Function('try {return this===window;}catch(e){ return false;}')
-	console.log(`${new Date()} isBrowser = ${isBrowser()};`)
-	assert.strictEqual(isBrowser(), true)
+describe('browser', function () {
+	it('isBrowser', function () {
+		// see: https://stackoverflow.com/a/31090240/5221762
+		const isBrowser = new Function('try {return this===window;}catch(e){ return false;}')
+		console.log(`${new Date()} isBrowser = ${isBrowser()};`)
+		assert.strictEqual(isBrowser(), true)
+	})
 })
